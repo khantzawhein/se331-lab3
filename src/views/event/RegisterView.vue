789d451850c70a48297e4e78b369307a0f3377ab
Khant Zaw Hein <kzh702@gmail.com>
2024-08-07 03:22:11 +0700

    4.2 done

--- src/views/event/RegisterView.vue
+++ src/views/event/RegisterView.vue
@@ -1,14 +1,21 @@
 <script setup lang="ts">
 import {toRefs} from "vue";
 import type {Event} from "@/type";
+import {useRouter} from "vue-router";
 
 const props = defineProps<{ event: Event , id: string}>();
 const {event} = toRefs(props);
-
+const router = useRouter();
+const register = () => {
+  router.push({name: 'event-detail-view'})
+}
 </script>
 
 <template>
   <p>Register event here</p>
+  <button @click="register">
+    Register
+  </button>
 </template>
 
 <style scoped>
